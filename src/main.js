0d02d7684a1501dde21cabb2f4eae5a03b33bc58
double2kill <379563000@qq.com>
2019-04-28 20:07:13 +0800

    Add: add element-ui

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import Vue from 'vue';
+import { Table, TableColumn } from 'element-ui';
 import App from './App.vue';
 import router from './router';
+
+
+// elementUI
+
+Vue.use(Table);
+Vue.use(TableColumn);
 
 Vue.config.productionTip = false;
 
---
+++ src/views/Table.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <Table/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Table from '@/components/Table.vue';
+
+export default {
+  name: 'home',
+  components: {
+    Table,
+  },
+};
+</script>
