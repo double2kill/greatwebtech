431884872f03125edf38a90de8ffa45dbbb5dc85
Mikelian <1543781582@qq.com>
2019-11-04 12:41:37 +0800

    修改工厂生产环境配置 	modified:   env/product.js

--- env/product.js
+++ env/product.js
@@ -1,8 +1,8 @@
 window.CONFIG = {
-  SEARCH_ORIGIN: 'http://www.greatwebtech.cn/search/',
+  SEARCH_ORIGIN: 'http://10.18.75.124/search/',
   // SEARCH_ORIGIN: backend url,
-  TYPE: 'have-home',
-  // TYPE: 'only-search',
+  //TYPE: 'have-home',
+   TYPE: 'only-search',
   // LOAD_DATA_ENTER_PAGE: true,
   LOAD_DATA_ENTER_PAGE: false,
 };
